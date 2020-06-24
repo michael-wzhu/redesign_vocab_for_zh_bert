8fb34d274dc5b5ed09826257adcb54f4c80ad025
michael <michael_wzhu91@163.com>
2020-06-24 17:46:00 +0800

    0624

--- src/char_segmented/create_pretrain_data.sh
+++ src/char_segmented/create_pretrain_data.sh
@@ -12,7 +12,7 @@ VOCAB_SIZE=21128
 
 
 # NUM_PROC=218
-NUM_PROC=2
+NUM_PROC=218
 
 
 
@@ -21,4 +21,4 @@ for i in `seq 1 $((NUM_PROC))`; do
   $@ &
 done
 
-# python3 src/char_spaced/create_pretraining_data.py --input_file=$STORAGE_BUCKET/data/corpus/${PREFIX}_lower/zhwiki-latest-pages-articles_100_${PREFIX}_lower_simplified.txt --output_file=${STORAGE_BUCKET}/experiments/rethink_vocab/pretrain_tfrecords/${PREFIX}_${VOCAB_SIZE}/zhwiki_train_examples_100_%s.tfrecord --do_lower_case=True --do_whole_word_mask=True --max_seq_length=128 --max_predictions_per_seq=13 --masked_lm_prob=0.1 --dupe_factor=2 --vocab_file data_proc/tokenizers/sentencepiece/${PREFIX}-${VOCAB_SIZE}-clean.vocab --spm_model_file data_proc/tokenizers/sentencepiece/${PREFIX}-${VOCAB_SIZE}-clean.model
+# python3 src/char_spaced/create_pretraining_data.py --input_file=gs://sbt0/data/corpus/char_segmented_lower/zhwiki-latest-pages-articles_100_char_segmented_lower_simplified.txt --output_file=gs://sbt0/experiments/rethink_vocab/pretrain_tfrecords/char_segmented_21128/zhwiki_train_examples_100_%s.tfrecord --do_lower_case=True --do_whole_word_mask=True --max_seq_length=128 --max_predictions_per_seq=13 --masked_lm_prob=0.1 --dupe_factor=2 --vocab_file data_proc/tokenizers/sentencepiece/char_segmented-21128-clean.vocab --spm_model_file data_proc/tokenizers/sentencepiece/char_segmented-21128-clean.model
