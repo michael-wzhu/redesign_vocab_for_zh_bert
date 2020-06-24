48d95a97f09301799a2e5c2b0b8b7af4c974fe4d
michael <michael_wzhu91@163.com>
2020-06-24 16:02:46 +0800

    0624

--- data_proc/merge_file_utils.py
+++ data_proc/merge_file_utils.py
@@ -46,62 +46,33 @@ if __name__ == "__main__":
         "data/corpus/char_no_space_lower/zhwiki-latest-pages-articles_char_no_space_lower_simplified.txt")
     txt_files2file(list_files_, to_dir_)
 
-    # list_files_ = []
-    # num_files = 218
-    # for i in range(num_files):
-    #     file_ = os.path.join(
-    #         STORAGE_BUCKET,
-    #         "data/corpus/subchar_segmented_lower/zhwiki-latest-pages-articles_%d_subchar_segmented_lower.txt" % (i + 1)
-    #     )
-    #     list_files_.append(file_)
-    #
-    # to_dir_ = os.path.join(
-    #     STORAGE_BUCKET,
-    #     "data/corpus/subchar_segmented_lower/zhwiki-latest-pages-articles_subchar_segmented_lower.txt")
-    # txt_files2file(list_files_, to_dir_)
+    list_files_ = []
+    num_files = 218
+    for i in range(num_files):
+        file_ = os.path.join(
+            STORAGE_BUCKET,
+            "data/corpus/char_segmented_lower/zhwiki-latest-pages-articles_%d_char_segmented_lower_simplified.txt" % (
+                        i + 1)
+        )
+        list_files_.append(file_)
 
-    # list_files_ = []
-    # num_files = 218
-    # for i in range(num_files):
-    #     file_ = os.path.join(
-    #         STORAGE_BUCKET,
-    #         "data/corpus/char_spaced_lower/zhwiki-latest-pages-articles_%d_char_spaced_lower.txt" % (i + 1)
-    #     )
-    #     list_files_.append(file_)
-    #
-    # to_dir_ = os.path.join(
-    #     STORAGE_BUCKET,
-    #     "data/corpus/char_spaced_lower/zhwiki-latest-pages-articles_char_spaced_lower.txt")
-    # txt_files2file(list_files_, to_dir_)
+    to_dir_ = os.path.join(
+        STORAGE_BUCKET,
+        "data/corpus/char_segmented_lower/zhwiki-latest-pages-articles_char_segmented_lower_simplified.txt")
+    txt_files2file(list_files_, to_dir_)
 
-    # list_files_ = []
-    # num_files = 110
-    # for i in range(num_files):
-    #     file_ = os.path.join(
-    #         STORAGE_BUCKET,
-    #         "data/corpus/subchar_lower/zhwiki-latest-pages-articles_%d_subchar_lower.txt" % (i + 1)
-    #     )
-    #     list_files_.append(file_)
-    #
-    # to_dir_ = os.path.join(
-    #     STORAGE_BUCKET,
-    #     "data/corpus/subchar_lower/zhwiki-latest-pages-articles_subchar_lower.txt")
-    # txt_files2file(list_files_, to_dir_)
+    list_files_ = []
+    num_files = 218
+    for i in range(num_files):
+        file_ = os.path.join(
+            STORAGE_BUCKET,
+            "data/corpus/char_spaced_lower/zhwiki-latest-pages-articles_%d_char_spaced_lower_simplified.txt" % (
+                    i + 1)
+        )
+        list_files_.append(file_)
 
-    # num_files = 110
-    # for i in range(num_files):
-    #     file_ = os.path.join(
-    #         STORAGE_BUCKET,
-    #         "data/corpus/char_lower/zhwiki-latest-pages-articles_%d_char_lower.txt" % (i + 1)
-    #     )
-    #     list_files_.append(file_)
-    #
-    # to_dir_ = os.path.join(
-    #     STORAGE_BUCKET,
-    #     "data/corpus/char_lower/zhwiki-latest-pages-articles_char_lower.txt")
-    # txt_files2file(list_files_, to_dir_)
+    to_dir_ = os.path.join(
+        STORAGE_BUCKET,
+        "data/corpus/char_spaced_lower/zhwiki-latest-pages-articles_char_spaced_lower_simplified.txt")
+    txt_files2file(list_files_, to_dir_)
 
-
-
-
-    
