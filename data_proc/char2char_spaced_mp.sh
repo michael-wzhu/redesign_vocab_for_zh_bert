2d1f89d5d972dfae393d1bcdb8819c52e58d3bf7
michael <michael_wzhu91@163.com>
2020-06-24 16:22:25 +0800

    0624

--- data_proc/char2char_spaced_mp.sh
+++ data_proc/char2char_spaced_mp.sh
@@ -5,6 +5,6 @@ export STORAGE_BUCKET=gs://sbt0
 ### multi-processing version
 NUM_PROC=218
 for i in `seq 1 $((NUM_PROC))`; do
-  python3 data_preprocess/char2char_spaced_mp.py ${STORAGE_BUCKET}/data/corpus/splited/zhwiki-latest-pages-articles_${i}.txt ${STORAGE_BUCKET}/data/corpus/char_spaced_lower/zhwiki-latest-pages-articles_${i}_char_spaced_lower_simplified.txt 1 \
+  python3 data_proc/char2char_spaced_mp.py ${STORAGE_BUCKET}/data/corpus/splited/zhwiki-latest-pages-articles_${i}.txt ${STORAGE_BUCKET}/data/corpus/char_spaced_lower/zhwiki-latest-pages-articles_${i}_char_spaced_lower_simplified.txt 1 \
     $@ &
 done
