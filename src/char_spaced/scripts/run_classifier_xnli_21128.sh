3efbd37aa01c99cb949f174ea43c233cb433575a
michael <michael_wzhu91@163.com>
2020-06-25 21:28:05 +0800

    prepare tfrecords for pretrain

---
+++ src/char_spaced/scripts/run_classifier_xnli_21128.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# @Author: Michael Zhu
+# @Date:   2020-03-04
+# @Last Modified by:   michael
+# @Last Modified time: 2020-03-04 21:28:30
+
+
+STORAGE_BUCKET=gs://sbt0
+TPU_IP=10.158.227.106
+TPU_NAME=grpc://${TPU_IP}:8470
+
+PREFIX=char_spaced
+VOCAB_SIZE=21128
+
+
+pip3 install tensorflow_hub
+pip3 install sentencepiece
+pip3 install jieba
+pip3 install tokenizers
+pip3 install boto3
+pip3 install jieba_fast
+pip3 install sklearn
+
+# run task
+
+echo "Start running..."
+RUN_TIMES=11
+for run_idx in `seq 1 $((RUN_TIMES))`; do
+
+    python3 src/char_spaced/run_classifier.py \
+      --task_name=xnli \
+      --data_dir=datasets/XNLI \
+      --output_dir=${STORAGE_BUCKET}/experiments/rethink_vocab/finetune/xnli/${PREFIX}_${VOCAB_SIZE}_length_128_steps_4.5k_time_0625_run_${run_idx}/ \
+      --init_checkpoint=${STORAGE_BUCKET}/experiments/rethink_vocab/pretraining/${PREFIX}_${VOCAB_SIZE}/model.ckpt-45000 \
+      --albert_config_file=./src/config_${VOCAB_SIZE}.json \
+      --do_train=true \
+      --do_eval=true \
+      --do_predict \
+      --do_lower_case \
+      --max_seq_length=128 \
+      --max_num_chars=128 \
+      --optimizer=adamw \
+      --train_batch_size=256 \
+      --eval_batch_size=32 \
+      --learning_rate=2e-5 \
+      --warmup_step=1000 \
+      --save_checkpoints_steps=1500 \
+      --train_step=30000 \
+      --use_tpu=True \
+      --tpu_name=${TPU_NAME} \
+      --num_tpu_cores=8 \
+      --vocab_file=./data_proc/tokenizers/sentencepiece/${PREFIX}-${VOCAB_SIZE}-clean.vocab \
+      --spm_model_file=./data_proc/tokenizers/sentencepiece/${PREFIX}-${VOCAB_SIZE}-clean.model
+
+
+done
+
