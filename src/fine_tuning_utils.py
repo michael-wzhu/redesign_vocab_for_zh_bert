fb83a73ef4c65bc201557f0a8360796fe311dbdc
michael <michael_wzhu91@163.com>
2020-06-25 15:36:31 +0800

    prepare tfrecords for pretrain

--- src/fine_tuning_utils.py
+++ src/fine_tuning_utils.py
@@ -19,10 +19,11 @@ This library can be used to construct ALBERT models for fine-tuning, either from
 json config files or from TF-Hub modules.
 """
 
-import modeling
-import tokenization
 import tensorflow.compat.v1 as tf
 import tensorflow_hub as hub
+
+from src import modeling
+from src import tokenization
 
 
 def _create_model_from_hub(hub_module, is_training, input_ids, input_mask,
