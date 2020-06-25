815e1eac4c3344834d963f059067d60946a1cc40
michael <michael_wzhu91@163.com>
2020-06-25 16:14:29 +0800

    prepare tfrecords for pretrain

--- src/char_no_space/scripts/run_classifier_lcqmc_21128.sh
+++ src/char_no_space/scripts/run_classifier_lcqmc_21128.sh
@@ -5,9 +5,12 @@
 # @Last Modified time: 2020-03-04 21:28:30
 
 
-export STORAGE_BUCKET=gs://sbt0
-# export TPU_NAME=h-bert-2
-export TPU_NAME=grpc://10.29.250.18:8470
+STORAGE_BUCKET=gs://sbt0
+TPU_IP=10.240.1.2
+TPU_NAME=grpc://${TPU_IP}:8470
+
+PREFIX=char_no_space
+VOCAB_SIZE=21128
 
 
 pip3 install tensorflow_hub
@@ -27,32 +30,27 @@ for run_idx in `seq 1 $((RUN_TIMES))`; do
     python3 src/run_classifier.py \
       --task_name=lcqmc \
       --data_dir=datasets/LCQMC \
-      --output_dir=${STORAGE_BUCKET}/experiments/h_bert/finetune/lcqmc_length_512_sememe_16_steps_125k_time_0514_1_run_${run_idx}/ \
-      --init_checkpoint=${STORAGE_BUCKET}/experiments/h_bert/pretraining/length_512_sememe_16_steps_125k_time_0507_0/model.ckpt-125000 \
-      --albert_config_file=./resources/albert_base_v2/config.json \
+      --output_dir=${STORAGE_BUCKET}/experiments/rethink_vocab/finetune/xnli/length_128_steps_4.5k_time_0625_run_${run_idx}/ \
+      --init_checkpoint=${STORAGE_BUCKET}/experiments/rethink_vocab/pretraining/${PREFIX}_${VOCAB_SIZE}/model.ckpt-45000 \
+      --albert_config_file=./src/config_${VOCAB_SIZE}.json \
       --do_train=true \
       --do_eval=true \
       --do_predict \
       --do_lower_case \
       --max_seq_length=128 \
-      --max_sememe_length=16 \
       --optimizer=adamw \
-      --train_batch_size=128 \
+      --train_batch_size=256 \
       --eval_batch_size=32 \
       --learning_rate=2e-5 \
-      --warmup_step=1500 \
-      --save_checkpoints_steps=2000 \
-      --train_step=30000 \
+      --warmup_step=700 \
+      --save_checkpoints_steps=1000 \
+      --train_step=15000 \
       --use_tpu=True \
       --tpu_name=${TPU_NAME} \
-      --num_tpu_cores=1 \
-      --bert_tokenizer_name bert-base-chinese \
-      --dict_word2sememes_dir resources/dict_word2sememes.json \
-      --dict_sememe2id_dir resources/dict_sememe2id.json \
-      --flashtext_dict_dir resources/dict_hownet_flashtext.json
+      --num_tpu_cores=8 \
+      --vocab_file=./data_proc/tokenizers/sentencepiece/${PREFIX}-${VOCAB_SIZE}-clean.vocab \
+      --spm_model_file=./data_proc/tokenizers/sentencepiece/${PREFIX}-${VOCAB_SIZE}-clean.model
 
 
 done
 
-
-# python src/vanilla_albert/run_classifier.py --task_name=lcqmc --data_dir=datasets/ChnSentiCorp --output_dir=./experiments/h_bert/finetune/chn_length_512_sememe_16_steps_125k_time_0512_0/ --init_checkpoint=experiments/zh_sample/model.ckpt-12 --albert_config_file=./resources/albert_base_v2/config.json --do_train=true --do_eval=true --do_predict=true --do_lower_case --max_seq_length=128 --max_sememe_length=16 --optimizer=adamw --train_batch_size=2 --learning_rate=2e-5 --warmup_step=20 --save_checkpoints_steps=30 --train_step=100 --bert_tokenizer_name bert-base-chinese --dict_word2sememes_dir resources/dict_word2sememes.json --dict_sememe2id_dir resources/dict_sememe2id.json --flashtext_dict_dir resources/dict_hownet_flashtext.json
