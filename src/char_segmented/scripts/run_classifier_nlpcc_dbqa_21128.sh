2ab5efbe33f682236956bed5b436846e2f975296
michael <michael_wzhu91@163.com>
2020-06-27 01:23:57 +0800

    prepare tfrecords for pretrain

--- src/char_segmented/scripts/run_classifier_nlpcc_dbqa_21128.sh
+++ src/char_segmented/scripts/run_classifier_nlpcc_dbqa_21128.sh
@@ -45,7 +45,7 @@ for run_idx in `seq 1 $((RUN_TIMES))`; do
       --learning_rate=2e-5 \
       --warmup_step=600 \
       --save_checkpoints_steps=750 \
-      --train_step=15000 \
+      --train_step=1000 \
       --use_tpu=True \
       --tpu_name=${TPU_NAME} \
       --num_tpu_cores=8 \
