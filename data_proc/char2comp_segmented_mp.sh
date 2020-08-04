88c869c7f64bca0112dc8f5eefd719fea8a49069
michael <michael_wzhu91@163.com>
2020-08-04 13:49:24 +0800

    prepare comp_bert tf records

--- data_proc/char2comp_segmented_mp.sh
+++ data_proc/char2comp_segmented_mp.sh
@@ -3,8 +3,8 @@
 export STORAGE_BUCKET=gs://sbt0
 
 ### multi-processing version
-NUM_PROC=100
-for i in `seq 1 $((NUM_PROC))`; do
+NUM_PROC=218
+for i in `seq 101 $((NUM_PROC))`; do
   python3 data_proc/char2comp_segmented_mp.py \
     ${STORAGE_BUCKET}/data/corpus/splited/zhwiki-latest-pages-articles_${i}.txt \
     ${STORAGE_BUCKET}/data/corpus/subchar_segmented_lower/zhwiki-latest-pages-articles_${i}_subchar_segmented_lower.txt \
