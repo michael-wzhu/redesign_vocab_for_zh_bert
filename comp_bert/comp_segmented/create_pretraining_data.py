b0aab1111e73da83d62355dd9c8f32250d90ab3c
michael <michael_wzhu91@163.com>
2020-08-05 15:14:37 +0800

    pretrain with vocab 21128

--- comp_bert/comp_segmented/create_pretraining_data.py
+++ comp_bert/comp_segmented/create_pretraining_data.py
@@ -263,7 +263,7 @@ def create_training_instances(input_files, tokenizer, max_seq_length,
     vocab_words = list(tokenizer.vocab.keys())
     instances = []
     for _ in range(dupe_factor):
-        for document_index in range(len(all_documents)):
+        for document_index in tqdm.tqdm(range(len(all_documents))):
             instances.extend(
                 create_instances_from_document(
                     all_documents, document_index, max_seq_length, short_seq_prob,
