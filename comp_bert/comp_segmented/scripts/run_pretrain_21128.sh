1dc5d58a7a1333e5d172e4eb6913681bf47c9ee9
michael <michael_wzhu91@163.com>
2020-08-06 16:48:38 +0800

    pretrain with vocab 21128

--- comp_bert/comp_segmented/scripts/run_pretrain_21128.sh
+++ comp_bert/comp_segmented/scripts/run_pretrain_21128.sh
@@ -33,5 +33,5 @@ python3 comp_bert/run_pretraining.py \
     --save_checkpoints_steps=3000
 
 
-# python comp_bert/run_pretraining.py --input_file=experiments/zh_sample/tf_records/zhwiki_examples_*.tfrecord --output_dir=experiments/zh_sample/pretraining/${PREFIX}_${VOCAB_SIZE}/ --albert_config_file=./comp_bert/albert_configs/config_${VOCAB_SIZE}.json --do_train --do_eval --dev_input_file=experiments/zh_sample/tf_records/zhwiki_examples_9.tfrecord --train_batch_size=2 --eval_batch_size=2 --max_seq_length=128 --max_predictions_per_seq=13 --optimizer="lamb" --learning_rate=2e-4 --num_train_steps=10 --num_warmup_steps=2 --save_checkpoints_steps=5
+# python comp_bert/run_pretraining.py --input_file=experiments/zh_sample/tf_records/zhwiki_examples_*.tfrecord --output_dir=experiments/zh_sample/pretraining/subchar_segmented_21128/ --albert_config_file=./comp_bert/albert_configs/config_21128.json --do_train --do_eval --dev_input_file=experiments/zh_sample/tf_records/zhwiki_examples_9.tfrecord --train_batch_size=2 --eval_batch_size=2 --max_seq_length=128 --max_predictions_per_seq=13 --optimizer="lamb" --learning_rate=2e-4 --num_train_steps=10 --num_warmup_steps=2 --save_checkpoints_steps=5
 
