5e14f912fa7892af5bbc0acd243c7871c157b835
michael <michael_wzhu91@163.com>
2020-08-07 20:45:36 +0800

    prepare tfrecords for pretrain

--- comp_bert/comp_segmented/scripts/create_pretrain_data_5282_11_55.sh
+++ comp_bert/comp_segmented/scripts/create_pretrain_data_5282_11_55.sh
@@ -26,8 +26,8 @@ for i in `seq 11 $((NUM_PROC))`; do
     --max_seq_length=512 \
     --max_predictions_per_seq=51 \
     --masked_lm_prob=0.1 \
-    --dupe_factor=1 \
+    --dupe_factor=10 \
     --vocab_file data_proc/tokenizers/sentencepiece/${PREFIX}_lower-${VOCAB_SIZE}-clean.vocab \
-    --spm_model_file data_proc/tokenizers/sentencepiece/${PREFIX}_lower-${VOCAB_SIZE}-clean.model \
-  $@ &
-done
+    --spm_model_file data_proc/tokenizers/sentencepiece/${PREFIX}_lower-${VOCAB_SIZE}-clean.model
+#   $@ &
+# done
