f94245928c6aa58287bf11d20afd138b4d3d1cdc
michael <michael_wzhu91@163.com>
2020-08-07 15:33:56 +0800

    pretrain with vocab 21128

--- comp_bert/comp_segmented/scripts/create_pretrain_data_21128_take0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-### multi-processing version
-
-./comp_bert/comp_segmented/scripts/create_pretrain_data_21128_56_72.sh
-./comp_bert/comp_segmented/scripts/create_pretrain_data_21128_73_89.sh
-
