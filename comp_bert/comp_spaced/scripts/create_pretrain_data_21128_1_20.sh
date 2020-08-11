f08c78cf6566f4bae6096ac9b0d7ec49d4eaa889
michael <michael_wzhu91@163.com>
2020-08-11 16:26:34 +0800

    pretrain with vocab 21128

--- comp_bert/comp_spaced/scripts/create_pretrain_data_21128_1_20.sh
+++ comp_bert/comp_spaced/scripts/create_pretrain_data_21128_1_20.sh
@@ -18,7 +18,7 @@ NUM_PROC=20
 
 
 for i in `seq 1 $((NUM_PROC))`; do
-  python3 comp_bert/comp_segmented/create_pretraining_data.py \
+  python3 comp_bert/comp_spaced/create_pretraining_data.py \
     --input_file=$STORAGE_BUCKET/data/corpus/${PREFIX}_lower/zhwiki-latest-pages-articles_${i}_${PREFIX}_lower.txt \
     --output_file=${STORAGE_BUCKET}/experiments/comp_bert/pretrain_tfrecords/${PREFIX}_${VOCAB_SIZE}/zhwiki_train_examples_${i}_%s.tfrecord \
     --do_lower_case=True \
