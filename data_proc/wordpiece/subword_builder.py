6d3a56a528c5a471774c0dba3bb4fa3de5dcaf65
michael <michael_wzhu91@163.com>
2020-09-25 10:11:48 +0800

    build vocab for char level

--- data_proc/wordpiece/subword_builder.py
+++ data_proc/wordpiece/subword_builder.py
@@ -32,8 +32,8 @@ from __future__ import absolute_import
 from __future__ import division
 
 import sys
-
 sys.path.append("./")
+
 from data_proc.wordpiece import text_encoder
 from data_proc.wordpiece import tokenizer
 
